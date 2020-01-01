2dd20cad7dc8fd094f55b27f8ed1016288c872a0
Shimpei Otsubo <potsbo@gmail.com>
2020-01-01 20:10:37 +0900

    Add simple calc

--- main.go
+++ main.go
@@ -20,16 +20,54 @@ func compile() error {
 		errors.New("Wrong size of arguments")
 	}
 
-	i, err := strconv.Atoi(os.Args[1])
+	fmt.Println(".intel_syntax noprefix")
+	fmt.Println(".global _main")
+	fmt.Println("_main:")
+
+	str, i, err := strtoint(os.Args[1])
 	if err != nil {
 		return err
 	}
 
-	fmt.Println(".intel_syntax noprefix")
-	fmt.Println(".global _main")
-	fmt.Println("_main:")
 	fmt.Printf("  mov rax, %d\n", i)
+	for {
+		if len(str) == 0 {
+			break
+		}
+		if str[0] == '+' {
+			str, i, err = strtoint(str[1:])
+			if err != nil {
+				return err
+			}
+			fmt.Printf("  add rax, %d\n", i)
+			continue
+		}
+
+		if str[0] == '-' {
+			str, i, err = strtoint(str[1:])
+			if err != nil {
+				return err
+			}
+			fmt.Printf("  sub rax, %d\n", i)
+			continue
+		}
+
+		return errors.New("Unexpected char")
+	}
 	fmt.Println("  ret")
 
 	return nil
+}
+
+func strtoint(str string) (string, int, error) {
+	cnt := 0
+	for _, char := range str {
+		if _, err := strconv.Atoi(string(char)); err == nil {
+			cnt++
+		} else {
+			break
+		}
+	}
+	i, err := strconv.Atoi(str[0:cnt])
+	return str[cnt:], i, err
 }
